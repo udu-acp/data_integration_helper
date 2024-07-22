afe0f79400ba8f1591303e05ff419f750a2f968b
udu-acp <joshua@amplifycp.com>
2024-07-22 10:09:31 -0500

    read columns.txt file and split columns into a list

---
+++ main.py
@@ -0,0 +1,25 @@
+# hardcode list of columns for conversion
+columns = [Company Code	Worked In Department	Worked In Department Description	Gross Pay	Net Pay	Take Home	
+           Regular Earnings Total	Regular Hours Total	Overtime Earnings Total	Overtime Hours Total	
+           AJJ_ADJUSTMENT_Earnings	BER_BEREAVEMENT_Earnings	BON_BONUS_Earnings	C19_C19Sick Leave_Earnings	
+           CLN_Call-In_Earnings	COM_COMMISSION_Earnings	FLO_FLOAT HOLIDAY_Earnings	HD2_HwkdyEOT_Earnings	
+           HOL_HOLIDAY_Earnings	HWE_HolWkdayEve_Earnings	HWN_HolWkdyNgh_Earnings	INS_INSERVICE_Earnings	
+           JUR_JURY DUTY_Earnings	NYT_NY Ten_Earnings	ONC_On-Call Pay_Earnings	OT2_Wkday Eve OT_Earnings	
+           OT3_WkdayNghtOT_Earnings	OT4_Wkend Eve OT_Earnings	OT5_WkendNght OT_Earnings	
+           PU1_7_3ShiftPickup_Earnings	PU2_3_11ShiftPickup_Earnings	PU3_11_7ShiftBonus_Earnings	
+           RET_RETRO_Earnings	RFB_REF BONUS_Earnings	SEV_SEVERANCE_Earnings	SIC_SICK_Earnings	
+           SNB_SIGN ON BONUS_Earnings	TRN_TRAIN/ORIENTATI_Earnings	VAC_VACATION_Earnings	
+           VPO_VAC PAYOUT_Earnings	WDE_Weekday Eve_Earnings	WDN_Weekday Night_Earnings	
+           WEE_Wkend Evening_Earnings	WEN_Wkend Night_Earnings	AJJ_ADJUSTMENT_Hours	
+           BER_BEREAVEMENT_Hours	BON_BONUS_Hours	C19_C19Sick Leave_Hours	COM_COMMISSION_Hours	
+           FLO_FLOAT HOLIDAY_Hours	HD2_HwkdyEOT_Hours	HOL_HOLIDAY_Hours	HWE_HolWkdayEve_Hours	
+           HWN_HolWkdyNgh_Hours	INS_INSERVICE_Hours	JUR_JURY DUTY_Hours	NYT_NY Ten_Hours	
+           ONC_On-Call Pay_Hours	OT2_Wkday Eve OT_Hours	OT3_WkdayNghtOT_Hours	OT4_Wkend Eve OT_Hours	
+           PU1_7_3ShiftPickup_Hours	PU2_3_11ShiftPickup_Hours	PU3_11_7ShiftBonus_Hours	
+           RET_RETRO_Hours	RFB_REF BONUS_Hours	SEV_SEVERANCE_Hours	SIC_SICK_Hours	SNB_SIGN ON BONUS_Hours	
+           TRN_TRAIN/ORIENTATI_Hours	VAC_VACATION_Hours	VPO_VAC PAYOUT_Hours	WDE_Weekday Eve_Hours	
+           WDN_Weekday Night_Hours	WEE_Wkend Evening_Hours	WEN_Wkend Night_Hours	4LN_401K LOAN_Deduction Amount	
+           71_TAX LEVY_Deduction Amount	72_TAX LEVY_Deduction Amount	73_GARNISHMENT_Deduction Amount	
+           74_GARNISHMENT_Deduction Amount	75_SUPPORT_Deduction Amount	ACC_ACCIDENT_Deduction Amount	
+           AUL_ALLSTATE U LIFE_Deduction Amount	CI1_Critical Illnes_Deduction Amount	CK1_CHECKING 1_Deduction Amount	CK2_CHECKING 2_Deduction Amount	CK3_CHECKING 3_Deduction Amount	CK4_CHECKING 4_Deduction Amount	CK5_CHECKING 5_Deduction Amount	DC1_FSA DEPENDENT_Deduction Amount	DIS_DISABILITY INSU_Deduction Amount	DN1_DENTAL_Deduction Amount	DPD_DOM PAR DENTAL_Deduction Amount	DPM_Dom Par Med PT_Deduction Amount	DPT_DENTAL AFTERTX_Deduction Amount	DPV_Dom Par Vision_Deduction Amount	EXP_EMPLOYEE EXP_Deduction Amount	FSA_FSA MEDICAL_Deduction Amount	GA2_GARNISHMENT$_Deduction Amount	HP1_Health Accident_Deduction Amount	K_401K_Deduction Amount	KCU_401K CATCHUP_Deduction Amount	MD1_MEDICAL_Deduction Amount	MDT_MEDICAL AFTERTX_Deduction Amount	MIS_MISCELLANEOUS_Deduction Amount	R_ROTH_Deduction Amount	SV1_SAVINGS 1_Deduction Amount	SV2_SAVINGS 2_Deduction Amount	SV3_SAVINGS 3_Deduction Amount	SV4_SAVINGS 4_Deduction Amount	TDL_TRADE LIFE_Deduction Amount	UVL_UNIVERSAL LIFE_Deduction Amount	VPT_VISION AFTERTX_Deduction Amount	VS1_VISION_Deduction Amount
+]
