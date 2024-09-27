59465f17d8349857c6bc5b6c487af4720999dc28
Joshua <joshua@amplifycp.com>
2024-09-27 13:04:16 -0500

    cleanup

--- generator.py
+++ generator.py
@@ -10,51 +10,6 @@ class ExtractionGenerator:
         openai.api_type = 'azure'
         openai.api_version = '2023-05-15'
         self.deployment_name = os.getenv("DEPLOYMENT_NAME")
-
-    def review_columns(self, input_file):
-        with open(input_file, 'r') as f:
-            input_content = f.read()
-
-        review_prompt = f"""
-        As an AI content reviewer, your task is to evaluate the following lines of code and extract database,
-        schema, table, column, and aliases from each line of code. The extraction should be usable for quick
-        documentation to allow for ease of understanding.
-
-        Text:
-        {input_content}
-
-        Please extract the compenents with the following in mind:
-        1. A namespace is the combination of database and schema listed as database.schema
-        2. A column can be written as database.schema.table.column AS alias
-        3. Column names can also be written as table.column or simply column, so you can work from right to left
-        4. The alias usually follows an AS, but always follows the column name
-
-        Your response should be returned in the following format:
-        Database, Schema, Table, Column, Alias (this will be the header and each subsequent row should return entries from the Text)
-        database_name, schema_name, table_name, column_name, alias_name
-
-
-        After generating the response, review it to ensure:
-        1. All provided information is in column form under database, schema, table, column, and alias
-        2. If a column's result is empty, instead of N/A use NULL
-        """
-
-        response = openai.ChatCompletion.create(
-            engine = self.deployment_name,
-            messages = [
-                {"role": "system", "content": "You are an expert code reviewer for documenting SQL logic and dependencies. Your task is to ensure all compenents of the logic are documented correctly"},
-                {"role": "user", "content": f"{review_prompt}"}
-            ],
-            max_tokens=500,
-            n=1,
-            stop=None,
-            temperature=0.2,
-        )
-
-        if response.choices and response.choices[0].message:
-            return response.choices[0].message.content.strip()
-        else:
-            return "Error: Unable to review content."
         
     def sql_extractor(self, input_file):
         with open(input_file, 'r') as f:
--- main.py
+++ main.py
@@ -8,23 +8,8 @@ from generator import ExtractionGenerator
 # table_extractor("input_columns.txt", "extracted_tables.txt")
 # column_counter("input_columns.txt", "extracted_counts.txt")
 
-# if input("Review or Document?") == "Review":
-#     extract_generator = ExtractionGenerator()
-
-#     input_path = "input_columns.txt"
-
-#     with open(input_path, 'rb') as f:
-#         input_content = f.read()
-
-#     data_documentation = extract_generator.review_text(input_content) 
-
-#     output_path = "extracted_ai_document.txt"
-#     with open(output_path, 'w') as f:
-#         f.write(data_documentation)
 
 
 extract_generator = ExtractionGenerator()
-
-# data_documentation = extract_generator.review_columns("input_columns.txt")
 data_documentation = extract_generator.sql_extractor("input_columns.txt")
 print(data_documentation)
